5de61335ff71b6b2b75de2d0fa463a64478271d7
Tales Garcia <garcia.tales@gmail.com>
2021-04-21 12:23:31 -0300

    feat(index page): fetch data using get static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import { GetStaticProps } from "next";
+
 export default function Home() {
   return (
     <h1>Index</h1>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const episodes = await (await fetch('http://localhost:3333/episodes')).json();
+
+  return {
+    props: {
+      episodes
+    },
+    revalidate: 60 * 60 * 4
+  };
 }
