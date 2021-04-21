8d8d5e893a8697c4079234cb179b9718b45cbe64
Tales Garcia <garcia.tales@gmail.com>
2021-04-21 07:11:13 -0300

    feat(app): apply global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <ThemeProvider theme={{}}>
         <Component {...pageProps} />
       </ThemeProvider>
