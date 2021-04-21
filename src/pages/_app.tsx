b32af7660f51d10773d7698470f86be4c782a31d
Tales Garcia <garcia.tales@gmail.com>
2021-04-21 07:40:47 -0300

    feat(app): create styled components theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,28 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/global";
 
+const theme = {
+  white: '#FFF',
+
+  gray50: '#F7F8FA',
+  gray100: '#E6E8EB',
+  gray200: '#AFB2B1',
+  gray500: '#808080',
+  gray800: '#494D4B',
+
+  green500: '#04D361',
+
+  purple300: '#9F75FF',
+  purple400: '#9164FA',
+  purple500: '#8257E5',
+  purple800: '#6F48C9'
+};
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={{}}>
+      <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
